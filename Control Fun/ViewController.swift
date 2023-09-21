119d063ab5939f827270f68a5f1adbbbf52c46b6
Tooga Garanja Magar <tugamagar77@gmail.com>
2023-09-20 21:04:15 -0400

    Initial Commit

---
+++ Control Fun/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Control Fun
+//
+//  Created by Tooga Garanja Magar on 2023-09-20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
